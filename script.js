e8b44d6c016d7e12b79cc18fcaca0745adf6fb65
hudson21 <camihudsondiaz@ittepic.edu.mx>
2025-09-28 13:44:07 -0400

    Added project files to respository

---
+++ script.js
@@ -0,0 +1,7 @@
+// Grab the button by its ID
+const button = document.getElementById("myButton");
+
+// Add a click event
+button.addEventListener("click", function () {
+  alert("Hello World, This is my first interactive website!");
+});
